33ddd395eb5ee451f483c2b15b8de13e9ab71bca
Julius Puentes <jfpuentes@gmail.comm>
2020-07-21 08:39:35 -0700

    Level 3 - Hashing with md5

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const md5 = require("md5");
 
 const app = express();
+
+console.log("weak password hash: " + md5("123456"));
+console.log("strong password hash: " + md5("sjkhdfsd8f7jhsd$%$sdfsdfHJKHSJFHDSF78324"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -38,37 +40,31 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser =  new User({
-      email: req.body.username,
-      password: hash
-    });
-    newUser.save(function(err){
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
+  const newUser =  new User({
+    email: req.body.username,
+    password: md5(req.body.password)
   });
-
+  newUser.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err) {
       console.log(err);
     } else {
       if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
-        });
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
       }
     }
   });
